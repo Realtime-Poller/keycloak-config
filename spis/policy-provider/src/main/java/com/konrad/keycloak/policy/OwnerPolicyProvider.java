39113cefde4c9f6a5bf8c85edb7d764485458ac5
konradeconomics <konradkomplexeleistung@gmail.com>
2025-09-26 15:47:02 +0200

    Syncing.

--- spis/policy-provider/src/main/java/com/konrad/keycloak/policy/OwnerPolicyProvider.java
+++ spis/policy-provider/src/main/java/com/konrad/keycloak/policy/OwnerPolicyProvider.java
@@ -10,7 +10,7 @@ public class OwnerPolicyProvider implements PolicyProvider {
 
     @Override
     public void evaluate(Evaluation evaluation) {
-        System.out.println("\n--- OwnerPolicyProvider: Evaluate method started ---");
+        System.out.println("\n--- OwnerPolicyProvider: Evaluate method started (DEBUG MODE) ---");
 
         var context = evaluation.getContext();
         var identity = context.getIdentity();
@@ -22,19 +22,27 @@ public class OwnerPolicyProvider implements PolicyProvider {
         System.out.println("[DEBUG] User ID from token (parsed) = '" + userId + "'");
 
         Map<String, Collection<String>> attributes = context.getAttributes().toMap();
+        System.out.println("[DEBUG] --- START OF ALL CONTEXT ATTRIBUTES ---");
+        attributes.forEach((key, value) ->
+                System.out.println("[DEBUG] Attribute Key: " + key + ", Value: " + value)
+        );
+        System.out.println("[DEBUG] --- END OF ALL CONTEXT ATTRIBUTES ---");
+
+
+        String claimKey = "owner";
         String owner = null;
 
-        if (attributes.containsKey("owner")) {
-            owner = attributes.get("owner").stream().findFirst().orElse(null);
+        if (attributes.containsKey(claimKey)) {
+            owner = attributes.get(claimKey).stream().findFirst().orElse(null);
         }
 
-        System.out.println("[DEBUG] Owner ID from request claims = '" + owner + "'");
+        System.out.println("[DEBUG] Owner ID from request claims (using " + claimKey + ") = '" + owner + "'");
 
         if (owner != null && owner.equals(userId)) {
             System.out.println("[SUCCESS] IDs match. Granting permission.");
             evaluation.grant();
         } else {
-            System.out.println("[FAILURE] IDs do not match. Denying permission.");
+            System.out.println("[FAILURE] IDs do not match or claim missing. Denying permission.");
         }
         System.out.println("--- OwnerPolicyProvider: Evaluate method finished ---\n");
     }
