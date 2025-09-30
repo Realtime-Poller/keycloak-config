8552881900e43bf52443148094aa09d273797644
konradeconomics <konradkomplexeleistung@gmail.com>
2025-09-30 10:30:23 +0200

    Final changes in policy provider.

--- spis/policy-provider/src/main/java/com/konrad/keycloak/policy/OwnerPolicyProvider.java
+++ spis/policy-provider/src/main/java/com/konrad/keycloak/policy/OwnerPolicyProvider.java
@@ -10,8 +10,6 @@ public class OwnerPolicyProvider implements PolicyProvider {
 
     @Override
     public void evaluate(Evaluation evaluation) {
-        System.out.println("\n--- OwnerPolicyProvider: Evaluate method started (DEBUG MODE) ---");
-
         var context = evaluation.getContext();
         var identity = context.getIdentity();
 
@@ -19,15 +17,7 @@ public class OwnerPolicyProvider implements PolicyProvider {
         String[] parts = keycloakUserId.split(":");
         String userId = parts.length > 0 ? parts[parts.length - 1] : null;
 
-        System.out.println("[DEBUG] User ID from token (parsed) = '" + userId + "'");
-
         Map<String, Collection<String>> attributes = context.getAttributes().toMap();
-        System.out.println("[DEBUG] --- START OF ALL CONTEXT ATTRIBUTES ---");
-        attributes.forEach((key, value) ->
-                System.out.println("[DEBUG] Attribute Key: " + key + ", Value: " + value)
-        );
-        System.out.println("[DEBUG] --- END OF ALL CONTEXT ATTRIBUTES ---");
-
 
         String claimKey = "owner";
         String owner = null;
@@ -36,15 +26,11 @@ public class OwnerPolicyProvider implements PolicyProvider {
             owner = attributes.get(claimKey).stream().findFirst().orElse(null);
         }
 
-        System.out.println("[DEBUG] Owner ID from request claims (using " + claimKey + ") = '" + owner + "'");
-
         if (owner != null && owner.equals(userId)) {
-            System.out.println("[SUCCESS] IDs match. Granting permission.");
             evaluation.grant();
         } else {
-            System.out.println("[FAILURE] IDs do not match or claim missing. Denying permission.");
+            evaluation.deny();
         }
-        System.out.println("--- OwnerPolicyProvider: Evaluate method finished ---\n");
     }
 
     @Override
